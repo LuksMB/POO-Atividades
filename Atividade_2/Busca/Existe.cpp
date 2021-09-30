7f8fe7067c1da6995dd335343cc2eeb3b375a5de
Lucas M. Braga <lucasmb.7@gmail.com>
2021-09-30 18:12:09 -0300

    Funcionando

---
+++ Atividade_2/Busca/Existe.cpp
@@ -0,0 +1,32 @@
+#include <cstdlib>
+#include <iostream>
+#include <vector>
+
+bool existir(int number, std::vector<int> arr)
+{
+    for (int x = 0; x < arr.size(); x++){
+        if (arr[x] == number)
+            return true;
+    }
+    return false;
+}
+
+int main(){ 
+    int n, tamanho;
+    std::cout << "Digite o numero a ser procurado: " << "\n";
+    std::cin >> n;
+    std::cout << "Digite o tamanho do vetor: " << "\n";
+    std::cin >> tamanho;
+    std::vector<int> arraia {};
+
+    for(int x = 0; x < tamanho; x++){
+        int a;
+        std::cout << "Digite a adicao ao vetor: " << "\n";
+        std::cin >> a; 
+        arraia.push_back(a);  
+    }
+    
+    bool boo = existir(n, arraia);
+    std::cout << boo << "\n";
+    return 0;
+}
